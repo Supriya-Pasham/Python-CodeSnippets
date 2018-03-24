22f03b5bb23edf302e88609d407a5bade53f57de
Supriya Pasham <supriya.pasham.consultant@nielsen.com>
2018-03-24 19:44:45 -0400

    Python code to find substring with overlapping occurances

---
+++ String_overlappingMatches.py
@@ -0,0 +1,12 @@
+# Assume s is a string of lower case characters.
+
+# Write a program that prints the number of times the string 'bob' occurs in s.
+# For example, if s = 'azcbobobegghakl', then your program should print
+
+
+s = "bobbobooeboobnbobboboobobbbobobovbtbobob"
+countBob = 0
+for i in range(len(s)):
+    if s[i:].startswith('bob'):
+        countBob += 1
+print("Number of times bob occurs is: "+ str(countBob))
